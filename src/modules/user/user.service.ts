f517aca70a6f96d3c3508a838743e42f3921130a
Algoruu <zlfkcmspdbdp@naver.com>
2025-01-04 02:39:21 +0900

    [Feat] 닉네임 중복 확인 로직 구현

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async checkNicknameAvailability(nickname: string): Promise<boolean> {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { nickname },
+    });
+
+    // 닉네임이 이미 존재하면 false 반환
+    return !existingUser;
+  }
+}
