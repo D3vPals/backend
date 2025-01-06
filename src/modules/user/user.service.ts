048c050c86c48f3c3f6b98c49b7a5baba419c380
Algoruu <zlfkcmspdbdp@naver.com>
2025-01-06 12:01:37 +0900

    [Feat] 이메일로 유저 찾기, 비밀번호 재설정 로직 userService에 추가

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -19,5 +20,19 @@ export class UserService {
       where: { email },
     });
     return user ? false : true; // 이메일이 이미 존재하면 false 반환
+  }
+
+  async findUserByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+  
+  async updatePasswordByEmail(email: string, newPassword: string): Promise<void> {
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+     await this.prisma.user.update({
+      where: { email },
+      data: { password: hashedPassword },
+    });
   }
 }
