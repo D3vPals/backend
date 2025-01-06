9d71591dd37f273989890ac67c5a7e2268222e43
Algoruu <zlfkcmspdbdp@naver.com>
2025-01-06 12:01:09 +0900

    [Fix] 프리즈마 전역 설정으로 개별 모듈에서 삭제

--- src/modules/authenticode/authenticode.module.ts
+++ src/modules/authenticode/authenticode.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthenticodeController } from './authenticode.controller';
 import { AuthenticodeService } from './authenticode.service';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [AuthenticodeController],
-  providers: [AuthenticodeService, PrismaService]
+  providers: [AuthenticodeService]
 })
 export class AuthenticodeModule {}
