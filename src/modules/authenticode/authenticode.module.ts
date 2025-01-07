7afff7ad8e1d1a331b1134a24030a280b8d6df98
Algoruu <zlfkcmspdbdp@naver.com>
2025-01-08 01:14:11 +0900

    [Fix] 이메일 모듈 받아오기 추가

--- src/modules/authenticode/authenticode.module.ts
+++ src/modules/authenticode/authenticode.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticodeController } from './authenticode.controller';
 import { AuthenticodeService } from './authenticode.service';
+import { EmailModule } from '../email/email.module';
 
 @Module({
+  imports: [EmailModule],
   controllers: [AuthenticodeController],
   providers: [AuthenticodeService]
 })
