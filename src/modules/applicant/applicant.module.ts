a4430b22ec2a3f8b5d81565ad7e6e5c33fcb5386
Algoruu <zlfkcmspdbdp@naver.com>
2025-01-08 01:09:47 +0900

    [Feat] 이메일과 인증 모듈 추가

--- src/modules/applicant/applicant.module.ts
+++ src/modules/applicant/applicant.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ApplicantController } from './applicant.controller';
 import { ApplicantService } from './applicant.service';
+import { EmailModule } from '../email/email.module';
+import { AuthModule } from '../auth/auth.module'; 
 
 @Module({
+  imports: [EmailModule, AuthModule],
   controllers: [ApplicantController],
   providers: [ApplicantService]
 })
