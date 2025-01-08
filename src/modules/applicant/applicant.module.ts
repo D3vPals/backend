816255e58934c2911b47a60190a6ff67de440a1c
Algoruu <zlfkcmspdbdp@naver.com>
2025-01-08 11:19:49 +0900

    [Feat] 프로젝트 모듈 추가

--- src/modules/applicant/applicant.module.ts
+++ src/modules/applicant/applicant.module.ts
@@ -3,9 +3,10 @@ import { ApplicantController } from './applicant.controller';
 import { ApplicantService } from './applicant.service';
 import { EmailModule } from '../email/email.module';
 import { AuthModule } from '../auth/auth.module'; 
+import { ProjectModule } from '../project/project.module'
 
 @Module({
-  imports: [EmailModule, AuthModule],
+  imports: [EmailModule, AuthModule, ProjectModule],
   controllers: [ApplicantController],
   providers: [ApplicantService]
 })
