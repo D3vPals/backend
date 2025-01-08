b9eaf3719fa2c3943fed4c9e871973b78ae6e3b8
Algoruu <zlfkcmspdbdp@naver.com>
2025-01-08 17:34:48 +0900

    [Fix]  v2 스타일의 API에서 v3 API로 수정

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -1,20 +1,20 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { S3 } from 'aws-sdk';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import * as crypto from 'crypto';
 
 @Injectable()
 export class UploadService {
-  private s3: S3;
+  private s3: S3Client;
   private bucketName: string;
 
   constructor(private configService: ConfigService) {
-    this.s3 = new S3({
+    this.s3 = new S3Client({
+      region: this.configService.get<string>('AWS_REGION'),
       credentials: {
         accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID'),
         secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
       },
-      region: this.configService.get<string>('AWS_REGION'),
     });
     this.bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME');
   }
@@ -27,16 +27,18 @@ export class UploadService {
   ): Promise<string> {
     try {
       const fileName = `${folder}/profile_${crypto.randomUUID()}.${fileType}`;
-      const uploadResult = await this.s3
-        .upload({
-          Bucket: this.bucketName,
-          Key: fileName,
-          Body: fileBuffer,
-          ContentType: `image/${fileType}`,
-        })
-        .promise();
+      const params = {
+        Bucket: this.bucketName,
+        Key: fileName,
+        Body: fileBuffer,
+        ContentType: `image/${fileType}`,
+      };
 
-      return uploadResult.Location;
+      await this.s3.send(new PutObjectCommand(params)); // v3 스타일 API 사용
+
+      return `https://${this.bucketName}.s3.${this.configService.get<string>(
+        'AWS_REGION',
+      )}.amazonaws.com/${fileName}`;
     } catch (error) {
       console.error('S3 Upload Error:', error);
       throw new InternalServerErrorException('S3 업로드 중 오류 발생');
